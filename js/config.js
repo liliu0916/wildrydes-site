b2bc66c39e083469d8040016811acc32172b04f5
Pelago <112896562+liliu0916@users.noreply.github.com>
2024-11-05 23:40:49 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_HdYiYXhde', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '617ffcq2cd6jcspd6nmcsjgv2g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'eu-north-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
