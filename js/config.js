c1b592f942ac48f86c4129b99394aad7c197d0d8
Pelago <112896562+liliu0916@users.noreply.github.com>
2024-11-05 23:44:18 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_HdYiYXhde', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '617ffcq2cd6jcspd6nmcsjgv2g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eu-north-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
